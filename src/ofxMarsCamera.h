f51b42ee0299d46ed8d6b85a664c3b06c311f60d
Moriya-Taichi <minecraft0125@gmail.com>
2020-05-18 08:22:02 +0900

    implement: header file

---
+++ src/ofxMarsCamera.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include "../Dependencies/IntTypes.h"
+#include "../Dependencies/SDK.h"
+
+using namespace ofxCv;
+using namespace cv;
+
+class MarsCamera: public ofThread {
+    
+    public:
+        ofImage cameraImage; // カメラの画像
+        void setThreadExecuteTime(int time);
+        void connect();
+        void setCameraValues(
+            double gamma, 
+            double gain, 
+            double shutterTime,
+            int brightness,
+            int contrast
+        );
+        void openStream();
+        void stopStream();
+        void disConnect();
+
+    private:
+        ofTimer timer;                          // スレッドの実行時
+        GENICAM_Camera *camera;                 // カメラのインスタンスのポインタ、
+        GENICAM_System *system;                 // カメラの接続数の取得、接続、切断を司るシステムのインスタンス
+        GENICAM_Camera *cameraList;             // APIでは接続してるカメラ全てが返ってくる
+        GENICAM_StreamSource *streamSource;     // 
+        GENICAM_ISPControl *ispControl;         // ~Controlは各種設定、cameraインスタンスから作る
+        GENICAM_AnalogControl *analogControl;
+        GENICAM_AcquisitionControl *acquisitionControl;
+        GENICAM_ImageFormatControl *imageFormarControl;
+        GENICAM_UserSetControl *userSetControl;
+
+        void setGamma(double gamma);
+        void setGain(double gain);
+        void setShutterTime(double shutterTime);
+        void setBrightness(int brightness);
+        void setContrast(int contrast);
+        void connectCamera();
+        void createControls();
+        void getWidthFromSystem();
+        void getHeightFromSystem();
+        void startCapture();
+        void stopCapture();
+        void disConnectCamera();
+};
